519b1a1664c6262d834f1c72e5ea11f3f3309c86
matthias-ennen <matthias.ennen@gmx.de>
2026-08-18 11:10:48 +0200

    Issue #29: Download-Infrastruktur vereinheitlichen

---
+++ app/src/test/java/com/whispercppdemo/TranscriptNotificationsTest.kt
@@ -0,0 +1,22 @@
+package de.matthiasennen.transcript
+
+import de.matthiasennen.transcript.download.TranscriptNotifications
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TranscriptNotificationsTest {
+    @Test
+    fun `background notifications have unique identifiers`() {
+        val ids = listOf(
+            TranscriptNotifications.WHISPER_MODEL_DOWNLOAD_ID,
+            TranscriptNotifications.TRANSCRIPTION_ID,
+            TranscriptNotifications.TRANSCRIPTION_COMPLETION_ID,
+            TranscriptNotifications.RECORDING_ID,
+            TranscriptNotifications.AI_PROCESSING_ID,
+            TranscriptNotifications.VAD_MODEL_DOWNLOAD_ID,
+            TranscriptNotifications.AI_MODEL_DOWNLOAD_ID
+        )
+
+        assertEquals(ids.size, ids.toSet().size)
+    }
+}
